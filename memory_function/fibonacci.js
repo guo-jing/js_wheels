96f26cb3528f4c95118d54372fe0f9f615e16ee7
guojing <343630774@qq.com>
2019-10-29 20:20:45 +0800

    同上

--- memory_function/fibonacci.js
+++ memory_function/fibonacci.js
@@ -1,25 +1,10 @@
-fibo = (n) => n === 1 || n === 2 ? 1 : fibo(n - 2) + fibo(n - 1);
-let cache = [];
-memorizedFibo = function (n) {
-    if (cache.length !== 0) {
-        for (let i = 0; i < cache.length; i++) {
-            if (cache[i].params === n) {
-                return cache[i].result;
-            }
-        }
-    }
-    let result;
-    if (n === 1 || n === 2) {
-        result = 1;
-    } else {
-        result = memorizedFibo(n-2) + memorizedFibo(n-1);
-    }
-    cache.push({
-        params: n,
-        result
-    });
-    return result
-};
+import memo from './main.js';
+
+function fibo(n) {
+    return n === 1 || n === 2 ? 1 : fibo(n - 2) + fibo(n - 1);
+}
+
+const memorizedFibo = memo(fibo);
 console.time('不记忆化的 Fibo');
 console.log(fibo(42));
 console.timeEnd('不记忆化的 Fibo');
--- memory_function/main.js
+++ memory_function/main.js
@@ -1,65 +1,13 @@
 const memo = (fn) => {
-    let fnString = fn.toString(); // 返回一个表示当前函数源代码的字符串
-    let reg1 = /^function\s*(\w+)\s*\(/;
-
-    if (reg1.exec(fnString) === null) {
+    if (typeof fn !== 'function') {
         // 没有匹配到函数函数表达式
         throw Error('fn is not a function');
     }
 
-    let functionName = reg1.exec(fnString)[1]; // 返回 reg1 的第一个分组，也就是函数名
-    let reg2String = `${functionName}(\\s*\\(|\\.call\\(|\\.apply\\()`; // 第一个 \ 为了在字符串中转义后面的 \
-    let reg2 = new RegExp(reg2String, 'g'); // 全局匹配 "fn( | fn.call( | fn.apply(" 的正则
-    if (reg2.exec(fnString) && reg2.exec(fnString) !== null) { // 连续执行两次 exec，第一次匹配的是函数名，第二次匹配的是方法中的递归调用
-        // 是递归
-        let reg3String = `function ${fn.name}\\s*\\(\\w*\\)\\s*\\{`;
-        let reg3 = new RegExp(reg3String);
-        let memorizedFuncString = fnString.replace(reg3, `$&
-        let cache = []; // 初始化缓存空间
-        return function(...args) {
-            
-            if (cache.length !== 0) {
-                for (let i = 0; i < cache.length; i++) {
-                    // 如果 cache[i] 中的参数长度和此次函数调用的参数的长度不相等，就没必要再比较具体参数
-                    if (cache[i].params.length !== args.length) {
-                        continue;
-                    } else {
-                        let j = 0;
-                        for (; j < args.length; j++) {
-                            if (cache[i].params[j] !== args[j]) {
-                                break;
-                            }
-                        }
-                        // 如果上面循环中每次参数比较都相等，就直接返回结果
-                        if (j === args.length) {
-                            return cache[i].result;
-                        }
-                    }
-                }
-            }
-            // 运行到此处说明此次调用是一个新参数列表
-            let result = fn.apply(this, args);
-            cache.push({
-                params: args,
-                result
-            });
-            return result
-        `);
-        let MemoFuncParams = [];
-        fn.arguments.forEach(function(arg){
-            MemoFuncParams.push(arg)
-        });
-        MemoFuncParams.push(memorizedFuncString);
-        let memorizedRecursion = new Function(...arguments, memorizedFuncString);
-        return memorizedRecursion;
-    } else {
-        // 不是递归
-        console.log('is not');
-    }
-
     let cache = []; // 初始化缓存空间
-    return function(...args) {
-
+    let fnCallingMemo; // fn 是递归 ? 一个函数 : undefined
+    let checkCache = function(...args) {
+        console.log('call checkCache');
         if (cache.length !== 0) {
             for (let i = 0; i < cache.length; i++) {
                 // 如果 cache[i] 中的参数长度和此次函数调用的参数的长度不相等，就没必要再比较具体参数
@@ -80,14 +28,56 @@ const memo = (fn) => {
             }
         }
         // 运行到此处说明此次调用是一个新参数列表
-        let result = fn.apply(this, args);
+        console.log('call fn');
+        let result = fnCallingMemo ? fnCallingMemo.apply(this, args) : fn.apply(this, args);
         cache.push({
             params: args,
             result
         });
         return result
+    };
+
+    let fnString = fn.toString(); // 返回一个表示当前函数源代码的字符串
+    const functionNameReg = /^function\s*(\w+)\s*\(\s*(.*)\s*\)/;
+    let functionName;
+    if (functionNameReg.exec(fnString) === null) {
+        // 箭头函数
+        return checkCache;
+    }
+    functionName = functionNameReg.exec(fnString)[1]; // 返回 reg1 的第一个分组，也就是函数名
+    let functionParams = functionNameReg.exec(fnString)[2];
+    let callingRegString = `\\W(${functionName})(\\s*\\(|\\.call\\(|\\.apply\\()`; // 第一个 \ 为了在字符串中转义后面的 \
+    const callingReg1 = new RegExp(callingRegString, 'g'); // 全局匹配 "fn( | fn.call( | fn.apply(" 的正则
+
+    if (callingReg1.exec(fnString) || callingReg1.exec(fnString) !== null) { // 连续执行两次 exec，第一次匹配的是函数名，第二次匹配的是方法中的递归调用
+        // 是递归
+        let memorizedFuncString = fnString;
+        let getFuncBodyRegStr = `function\\s*${functionName}\\s*\\(.*\\)\\s*\\{((.|\\n)+)\\}`;
+        const getFuncBodyReg = new RegExp(getFuncBodyRegStr); // 用来截取 fn 函数体的正则
+        memorizedFuncString = getFuncBodyReg.exec(memorizedFuncString)[1]; // 获得 fn 的函数体部分
+        const callingReg2 = new RegExp(callingRegString, 'g');
+        let resultArr;
+        while((resultArr = callingReg2.exec(memorizedFuncString)) !== null) {
+            let firstStr = memorizedFuncString.substring(0, resultArr.index - 1);
+            let endStr = memorizedFuncString.substring(resultArr.index + resultArr[1].length);
+            memorizedFuncString = firstStr + resultArr[1] + endStr;
+        }
+        let MemoFuncParams = [];
+        functionParams.split(/\s*,\s*/).forEach(function(args){
+            MemoFuncParams.push(args)
+        });
+        MemoFuncParams[0] === ''
+            ? MemoFuncParams = [memorizedFuncString]
+            : MemoFuncParams.push(memorizedFuncString);
+        fnCallingMemo = new Function(...MemoFuncParams);
+        return fnCallingMemo;
+    } else {
+        // 不是递归
+        return checkCache;
     }
 };
+
+export default memo;
 
 const x2 = memo((x) => {
     console.log('执行了一次')
@@ -99,4 +89,3 @@ console.log(x2(1)) // 打印出执行了，并且返回2
 console.log(x2(1)) // 不打印执行，并且返回上次的结果2
 // 第三次调用 x2(1)
 console.log(x2(1)) // 不打印执行，并且返回上次的结果2
-//测试
